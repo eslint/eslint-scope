2a7a7a6b8691a9e496214745c3f51d5bb34f3ac5
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2021-11-16 09:31:37 -0800

    feat: Add sourceType:commonjs support

--- tests/nodejs-scope.js
+++ tests/nodejs-scope.js
@@ -28,13 +28,40 @@ import espree from "./util/espree.js";
 import { analyze } from "../lib/index.js";
 
 describe("nodejsScope option", () => {
-    it("creates a function scope following the global scope immediately", () => {
+
+    it("creates a function scope following the global scope immediately when nodejscope: true", () => {
         const ast = espree(`
             "use strict";
             var hello = 20;
         `);
 
         const scopeManager = analyze(ast, { ecmaVersion: 6, nodejsScope: true });
+
+        expect(scopeManager.scopes).to.have.length(2);
+
+        let scope = scopeManager.scopes[0];
+
+        expect(scope.type).to.be.equal("global");
+        expect(scope.block.type).to.be.equal("Program");
+        expect(scope.isStrict).to.be.false;
+        expect(scope.variables).to.have.length(0);
+
+        scope = scopeManager.scopes[1];
+        expect(scope.type).to.be.equal("function");
+        expect(scope.block.type).to.be.equal("Program");
+        expect(scope.isStrict).to.be.true;
+        expect(scope.variables).to.have.length(2);
+        expect(scope.variables[0].name).to.be.equal("arguments");
+        expect(scope.variables[1].name).to.be.equal("hello");
+    });
+
+    it("creates a function scope following the global scope immediately when sourceType:commonjs", () => {
+        const ast = espree(`
+            "use strict";
+            var hello = 20;
+        `);
+
+        const scopeManager = analyze(ast, { ecmaVersion: 6, sourceType: "commonjs" });
 
         expect(scopeManager.scopes).to.have.length(2);
 
